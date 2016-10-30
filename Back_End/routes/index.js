ca3fe53572613646d35dd27841cbe0c797af87a8
malancas <malancaster0@gmail.com>
2016-10-30 23:07:08 +0000

    Changed subdirectory makeup

--- Back_End/routes/index.js
+++ Back_End/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var User = require("../models/userSchema");
+var Question = require("../models/questionSchema");
 
 // api routes //
 /* GET home page. */
@@ -11,36 +12,103 @@ router.get('/', function(req, res, next) {
 
 // Log user in, assume app is already connected to the database
 router.put('/addQuestion', addQuestion);
+router.delete('/deleteQuestion', deleteQuestion);
+router.get('/viewQuestion', getQuestion);
+router.put('/register', register);
+router.post('/login', login);
 
 function addQuestion(req, res){
   if (!req.body) res.json({"status" : "fail",
                                    "data" : "No data sent"});
-  User.findOne({ "username" : res.body.username });
+  User.findOne({ "username" : res.user.username });
   if (!User){
-    return
-    ({
+    res.json({
       "status" : "fail",
       "data" : { "username" : "User not found" }
     });
   }
   
   // Make unique id
-  {$push : {
-    User.questions : {
-      "name" : res.body.qname,
-      "description" : res.body.qdescription,
-      "category" : res.body.qcategory
-      //"id" : generate unique id
-    }
-  }}
+  var newQuestion = {"name" : req.body.qname, "description" : req.body.qdescription, "category" : req.body.qcategory};
+  Users.findOneAndUpdate({username: req.user.username}, {$push: {questions: newQuestion}});
   
-  return
-  ({
+  res.json({
     "status" : "success",
     "data" : {
       "put" : { "name" : res.body.qname, "description" : res.body.qdescription, "category" : res.body.qcateogry }
     }
   });
+}
+
+function deleteQuestion(req, res){
+  console.log('Delete question...nothing here yet');
+}
+
+function viewQuestion(req, res){
+  console.log('View question...nothing here yet');
+}
+
+function register(req, res){
+  if (!req.body) return res.json({"status" : "fail",
+                                   "data" : "No data sent"});
+  else if (!User.findOne({"username" : req.user.username})){
+    res.json({
+      "status" : "fail",
+      "data" : { "username" : "Username is already taken" }
+    });
+  }
+  else {
+    var newUser = new User ({
+      "username" : req.user.username,
+      "password" : req.user.password,
+      "admin" : false
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log('Error adding user to database');
+        res.json({
+          "status" : "error",
+          "data" : "User couldn't be saved"
+        });
+      }
+      res.json({
+        "status" : "success",
+        "data" : {
+          "post" : {"username" : req.user.username, 
+                    "password" : req.user.password}
+        }
+      });
+    });
+  }
+}
+
+function login(req, res){
+  if (!req.body) {
+    res.json({
+      "status" : "fail",
+      "data" : "No data sent"
+    });
+  }
+  else if (!User.findOne({"username" : req.user.username})){
+    res.json({
+      "status" : "fail",
+      "data" : { "username" : "Username not found" }
+    });
+  }
+  else if (!User.findOne({"username" : req.user.username, "password" : req.user.password})){
+    res.json({
+      "status" : "fail",
+      "data" : { "Password" : "Incorrect password" }
+    });
+  }
+  else {
+    res.json({
+      "status" : "success",
+      "data" : { 
+        "post" : { "username" : uname, "password" : pword }
+      }
+    });
+  }
 }
 
 module.exports = router;
